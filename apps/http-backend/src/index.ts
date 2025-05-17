afb8641ed34364e6f89ba4976d4b90bf747a7cfd
Anurag Ojha <aojharaj2004@gmail.com>
2025-05-17 14:59:24 +0530

    db

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,19 +1,36 @@
 import express from "express"
 
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "./config"
+import { JWT_SECRET } from "@repo/backend-common/config"
 import { middleware } from "./middleware"
-
+import {CreateUserSchema,SigninSchema,CreateRoomSchema} from "@repo/common/types"
 const app=express()
 
 
 app.post("/signup",(req,res)=>{
+
+    const data=CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return
+    }
     res.json({
         userId:"123"
     })
 
 })
 app.post("/signin",(req,res)=>{
+
+    const data=SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return
+    }
+    
     const userId=1;
     const token=jwt.sign({
         userId
@@ -25,6 +42,18 @@ app.post("/signin",(req,res)=>{
     
 })
 app.post("/room",middleware,(req,res)=>{
+
+    const data=CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return
+    }
+    res.json({
+        userId:"123"
+    })
+
     res.json({
         roomId:123
     })
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,21 +1,23 @@
 import { NextFunction,Request,Response } from "express";
-import { JWT_SECRET } from "./config";
-import jwt from "jsonwebtoken";
 
-export function middleware(req:Request,res:Response,next:NextFunction){
+import { JWT_SECRET } from "@repo/backend-common/config";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+
+interface customRequest extends Request {
+  userId?: string;
+}
+export function middleware(req:customRequest,res:Response,next:NextFunction){
 
     const token=req.headers["authorization"]??""
 
-    const decoded=jwt.verify(token,JWT_SECRET)
-
-    if(decoded){
-        //@ts-ignore
-        req.userId=decoded.userId
-        next();
-    }else{
-        res.status(403).json({
-            message:"Unauthorized"
-        })
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload & {userId:string} ;
+        
+        req.userId = decoded.userId;
+        next(); 
+    } catch (e) {
+        res.status(403).json({ message: "Unauthorized" });
     }
 
 }
