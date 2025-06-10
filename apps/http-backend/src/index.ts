8f09b7ca196bd9e36c1ddbf983cb930bdc55edf9
Anurag Ojha <aojharaj2004@gmail.com>
2025-06-10 12:17:36 +0530

    dashboard

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -15,36 +15,35 @@ app.post("/signup", async (req, res) => {
 
     const parsedData = CreateUserSchema.safeParse(req.body);
     if (!parsedData.success) {
-        console.log(parsedData)
-        console.log(parsedData.error);
+        
         res.json({
             message: "Incorrect inputs"
         })
         return;
     }
-    console.log(parsedData.data.email)
-    console.log(parsedData.data.name)
-    console.log(parsedData.data.password)
 
     try {
         const hashedPassword=await bcrypt.hash(parsedData.data.password,10)
+       
         const user = await prismaClient.user.create({
             
             data: {
-                email: parsedData.data?.email,
+                email: parsedData.data.email,
                 password: hashedPassword,
                 name: parsedData.data.name
             }
         })
-        console.log(user.id)
+        
         res.json({
             userId: user.id
             
         })
     } catch(e) {
-        res.status(411).json({
-            message: "User already exists with this username"
-        })
+        
+        res.status(500).json({
+            message: "Signup failed",
+             error: e instanceof Error ? e.message : String(e)
+        });
     }
 })
 
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,7 +1,7 @@
 import {z} from "zod"
 
 export const CreateUserSchema=z.object({
-    username:z.string().min(3).max(20),
+    username:z.string().min(3).max(20).optional(),
     password:z.string(),
     name:z.string(),
     email:z.string(),
